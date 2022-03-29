9a7d332af961257f0b885a32d097b6071f48ce55
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-28 20:22:50 -0700

    fill out migrations

---
+++ database/migrations/2022_03_29_025817_create_manufacturers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('manufacturers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+			$table->string('name');
+			$table->string('salesphone');
+			$table->string('salesemail');
+			$table->string('techphone');
+			$table->string('techemail');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('manufacturers');
+    }
+};
