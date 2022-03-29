07ab068f68bfd33e7a9961b813f7be9bf137d827
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-28 21:56:52 -0700

    Create create.blade.php

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+
+@section('title', 'Users')
+
+@section('content_header')
+    <h1>Users</h1>
+@stop
+
+@section('content')
+<form method="post" action="{{ route('uzers.store') }}" >
+    @csrf
+    <x-adminlte-input name="name" label="Name" />
+    <x-adminlte-input name="email" label="Email" />
+	<x-adminlte-input name="phone" label="Phone Number" />
+	<x-adminlte-input name="email" label="email" />
+    <x-adminlte-button type="Submit" label="Submit" />
+</form>
+@stop
