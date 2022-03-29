3b1db4a2f215a3b192c1c33bcca972e9562a8d0d
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-28 21:14:19 -0700

    aaa

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,19 +1,41 @@
 @extends('adminlte::page')
 
-@section('title', 'title')
+@section('title', 'User List')
 
 @section('content_header')
-    <h1>Header</h1>
+    <h1>To Do's</h1>
 @stop
 
 @section('content')
-    <p>Content here</p>
-@stop
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Task</th><th>Progress</th><th style="width: 40px">Action</th>
+        </tr>
+      </thead>
+      <tbody>
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+        @foreach($uzers AS $uzer)
+        <tr>
+          <td>{{ $uzer->id }}</td>
+          <td>{{ $uzer->name }}</td>
+          <td><a class="btn btn-default btn-sm" href="{{ route('users.show',['uzer'=>$uzer->id]) }}">View</a></td>
+        </tr>
+        @endforeach
+
+      </tbody>
+    </table>
+  </div>
+</div>
+<a href="{{ route('users.create') }} " class="btn btn-primary" >Create</a>
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+<script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
+</script>
 @stop
