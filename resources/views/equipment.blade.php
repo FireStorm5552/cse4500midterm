fdb18371ce943302a762be150bc9b2aff1909125
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-30 16:20:07 -0700

    Update equipment.blade.php

--- resources/views/equipment.blade.php
+++ resources/views/equipment.blade.php
@@ -1,19 +1,41 @@
 @extends('adminlte::page')
 
-@section('title', 'title')
+@section('title', 'Equipment List')
 
 @section('content_header')
-    <h1>Header</h1>
+    <h1>Equipment</h1>
 @stop
 
 @section('content')
-    <p>Content here</p>
-@stop
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+        @foreach($equipment)
+        <tr>
+          <td>{{ $equipment->id }}</td>
+          <td>{{ $equipment->name }}</td>
+          <td><a class="btn btn-default btn-sm" href="{{ route('equipment.show',['equipment'=>$equipment->id]) }}">View</a></td>
+        </tr>
+        @endforeach
+
+      </tbody>
+    </table>
+  </div>
+</div>
+<a href="{{ route('equipment.create') }} " class="btn btn-primary" >Create</a>
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+<script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
+</script>
 @stop
