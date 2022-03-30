216b9e0dc764402fa193fde186202d84493a42df
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-29 20:12:43 -0700

    Create 2022_03_29_025832_create_notes_table.php

---
+++ database/migrations/2022_03_29_025832_create_notes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('equipment_id')->constrained("equipment");
+			$table->string('services');
+			$table->string('software');
+			$table->string('notes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+};
