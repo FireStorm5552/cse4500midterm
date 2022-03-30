4f69a39cdaff2bc4832ba846dd8547ee470a7a30
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-29 21:20:16 -0700

    Update show.blade.php

--- resources/views/uzers/show.blade.php
+++ resources/views/uzers/show.blade.php
@@ -10,9 +10,9 @@
   <h2>{{ $uzer->name; }}</h2>
   <h2>{{ $uzer->email; }}</h2>
   <h2>{{ $uzer->phone; }}</h2>
-  <a href="{{ route('uzer.destroy',['uzer'=>$uzer->id]) }}" class="btn btn-danger" onclick="event.preventDefault(); document.getElementById('submit-form').submit();">
+  <a href="{{ route('uzers.destroy',['uzer'=>$uzer->id]) }}" class="btn btn-danger" onclick="event.preventDefault(); document.getElementById('submit-form').submit();">
   Delete</a>
-  <form id="submit-form" action="{{ route('uzer.destroy',['uzer'=>$uzer->id]) }}" method="POST" class="hidden">
+  <form id="submit-form" action="{{ route('uzers.destroy',['uzer'=>$uzer->id]) }}" method="POST" class="hidden">
     @csrf
     @method('DELETE')
 @stop
