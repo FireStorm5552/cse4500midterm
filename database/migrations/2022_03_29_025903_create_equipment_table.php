02ed17696e93f456378c88bc935b680f6e1ac67b
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-29 20:10:51 -0700

    Create 2022_03_29_025903_create_equipment_table.php

---
+++ database/migrations/2022_03_29_025903_create_equipment_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipment', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+			$table->string('name');
+			$table->string('processor');
+			$table->string('ram');
+			$table->string('type');
+			$table->foreignId('manufacturer_id')->constrained("manufacturers");
+			$table->foreignId('purchase_id')->constrained("purchases");
+			$table->foreignId('user_id')->constrained("uzers");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipment');
+    }
+};
