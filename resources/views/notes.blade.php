46dd80218dfd92a5103259096f649f7e38ebf377
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-30 18:16:57 -0700

    a

--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -1,19 +1,41 @@
 @extends('adminlte::page')
 
-@section('title', 'title')
+@section('title', 'Equipment List')
 
 @section('content_header')
-    <h1>Header</h1>
+    <h1>Equipment</h1>
 @stop
 
 @section('content')
-    <p>Content here</p>
-@stop
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+        @foreach($notes AS $note)
+        <tr>
+          <td>{{ $note->id }}</td>
+          <td>{{ $note->equipment->name }}</td>
+          <td><a class="btn btn-default btn-sm" href="{{ route('note.show',['note'=>$note->id]) }}">View</a></td>
+        </tr>
+        @endforeach
+
+      </tbody>
+    </table>
+  </div>
+</div>
+<a href="{{ route('note.create') }} " class="btn btn-primary" >Create</a>
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+<script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
+</script>
 @stop
---
+++ resources/views/notes/show.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title', 'Equipment')
+
+@section('content_header')
+    <h1>Equipment</h1>
+@stop
+
+@section('content')
+  <h2>Name: {{ $equipment->name; }}</h2>
+  <h2>Processor: {{ $equipment->processor; }}</h2>
+  <h2>RAM: {{ $equipment->ram; }}</h2>
+  <h2>Type: {{ $equipment->type; }}</h2>
+  <h2>Manufacturer: {{ $equipment->manufacturer->name; }}</h2>
+  <h2>Invoice Number: {{ $equipment->purchase->invoice; }}</h2>
+  <h2>Assigned User: {{ $equipment->uzer->name; }}</h2>
+  <a href="{{ route('equipment.edit', ['equipment'=>$equipment->id]) }} " class="btn btn-warning">Update</a>
+  <a href="{{ route('equipment.destroy',['equipment'=>$equipment->id]) }}" class="btn btn-danger" onclick="event.preventDefault(); document.getElementById('submit-form').submit();">
+  Delete</a>
+  <form id="submit-form" action="{{ route('equipment.destroy',['equipment'=>$equipment->id]) }}" method="POST" class="hidden">
+    @csrf
+    @method('DELETE')
+@stop
