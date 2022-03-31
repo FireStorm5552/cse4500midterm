ba56942e111947bb858d8f8bedbb63eb8c19c196
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-30 17:30:46 -0700

    Update EquipmentForm.php

--- app/Forms/EquipmentForm.php
+++ app/Forms/EquipmentForm.php
@@ -31,7 +31,7 @@ function getPurchases() {
     $retVal = array();
     $purchases = Purchase::where('id' ,'>' ,0)->get();
     foreach($purchases as $purchase) {
-        $retVal[strval($purchase["id"])] = strval($purchase["name"]);
+        $retVal[strval($purchase["id"])] = strval($purchase["ivoice"]);
     }
     return $retVal;
 }
