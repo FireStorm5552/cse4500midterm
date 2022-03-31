19a4f3b927e08ad2ded802c07c49a3a494435abe
Nathan Anderson <nathanranderson5973@gmail.com>
2022-03-30 18:12:35 -0700

    Update NoteController.php

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Notes;
+use App\Models\Note;
 use Kris\LaravelFormBuilder\FormBuilder;
 use App\Forms\NotesForm;
 class NoteController extends Controller
